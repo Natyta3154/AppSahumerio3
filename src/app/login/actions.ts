4a39d685192c6648b3af795f2df08fd3ec52748a
Hernan Fernandez <Herny3154@gmail.com>
2025-09-30 19:35:50 +0000

    actualizacion de sesion

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -29,7 +29,7 @@ export async function loginAction(
 
   let responseData;
   try {
-    const response = await fetch('/api/usuarios/login', {
+    const response = await fetch('/usuarios/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
